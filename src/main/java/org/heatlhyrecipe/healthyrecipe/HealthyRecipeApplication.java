f9fc4e5bf79f25206d8dac5a40b5444e61f22394
Laa <lastembo@gmail.com>
2020-03-18 19:13:34 -0500

    initial spring bot gradle build with MySQL datasource healthy-recipe

---
+++ src/main/java/org/heatlhyrecipe/healthyrecipe/HealthyRecipeApplication.java
@@ -0,0 +1,13 @@
+package org.heatlhyrecipe.healthyrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/heatlhyrecipe/healthyrecipe/HealthyRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heatlhyrecipe.healthyrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
